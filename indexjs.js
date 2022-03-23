2157777a95ea838f7a17cc21c61908c0f8c6b2f5
lucaciriaco <33738825+lucaciriaco@users.noreply.github.com>
2022-03-23 19:23:05 -0300

    .

--- indexjs.js
+++ indexjs.js
@@ -1,15 +1,15 @@
-
 function calculadoraPromedios(){
     let option = true;
-    let nota = new Number(0);
+    let nota = 0;
     let i = 0;
     let promedio = 0;
-    while(option==true){
-        nota = nota += parseInt(prompt("Ingrese valor de la nota:"),10);
+    while(option){
+        nota += parseInt(prompt("Ingrese valor de la nota:"),10);
         i++;
         option = confirm("Desea cargar otra nota?");
     }
     promedio = nota / i;
     alert("El promedio total de las notas es: " + promedio);
     document.write("<br> El promedio total de las notas es: " + promedio);
+    document.write("<h6>Hecho por Luca Ciriaco para <a href='https://www.coderhouse.com/'>Coderhouse</a></h6>");
 }
