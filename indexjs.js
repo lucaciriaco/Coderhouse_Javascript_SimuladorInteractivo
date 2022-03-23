ce24f0de90735ff3a954a92bbf9f7af6b486ab28
lucaciriaco <33738825+lucaciriaco@users.noreply.github.com>
2022-03-22 21:11:49 -0300

    first commit

---
+++ indexjs.js
@@ -0,0 +1,14 @@
+
+function calculadoraPromedios(){
+    let option = true;
+    let nota = new Number(0);
+    let i = 0;
+    let promedio = 0;
+    while(option==true){
+        nota = nota += parseInt(prompt("Ingrese valor de la nota:"),10);
+        i++;
+        option = confirm("Desea cargar otra nota?");
+    }
+    promedio = nota / i;
+    alert("El promedio de las notas es: " + promedio);
+}
