92f804fb21d28236b008bd3d2986fb98a4df0b34
lucaciriaco <33738825+lucaciriaco@users.noreply.github.com>
2022-03-23 21:47:33 -0300

    .

--- indexjs.js
+++ indexjs.js
@@ -9,6 +9,7 @@ function calculadoraPromedios(){
         option = confirm("Desea cargar otra nota?");
     }
     promedio = nota / i;
+    return promedio;
     alert("El promedio total de las notas es: " + promedio);
     document.write("<br> El promedio total de las notas es: " + promedio);
     document.write("<h6>Hecho por Luca Ciriaco para <a href='https://www.coderhouse.com/'>Coderhouse</a></h6>");
